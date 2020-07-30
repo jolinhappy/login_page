e894ae19c5ffd0a87162a14168f5cc2e49998c35
Phoebe <jolinhappy1990@hotmail.com>
2020-07-30 12:20:01 +0800

    feat: add body-parser

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
   res.render('index')
 })
 
